01b2af1c9352f52c1993ec663d6ed605525a8219
santospanda111 <santospanda111@gmail.com>
2021-04-26 22:40:52 +0530

    first commit added 1st code

---
+++ empwage/src/empwage/Empwage.java
@@ -0,0 +1,15 @@
+package empwage;
+
+public class Empwage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	      int IS_FULL_TIME=1;
+	      double empcheck=Math.floor(Math.random() * 10) % 2;
+	      if (empcheck == IS_FULL_TIME)
+	         System.out.println("Employee is present");
+	      else
+	         System.out.println("Employee is absent");
+	}
+
+}
